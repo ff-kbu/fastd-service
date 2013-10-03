89998921f01d3cab6267e7b05a170b8023fc9cff
yanosz <yanosz@gmx.net>
2013-10-03 10:17:53 +0200

    Update fastd_service.rb
    Busybox / wget thinks, HTTP-201 is an error.

--- fastd_service.rb
+++ fastd_service.rb
@@ -26,8 +26,8 @@ get '/upload_key' do
   if(params[:_method] == 'post')
     begin
       @@service.process_key_upload(params)
-      status 201 #Created
-      '<h1>201 Created</h1>'
+      status 200 #Created
+      '<h1>200 Created</h1>'
     rescue Exception => e
       status 422 #Unprocessable Entity
       "<h1>422 Unprocessable Entity</h1><br />#{e}\n"
